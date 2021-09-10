10b5e6b0a9fce0cf6db7cd7e6004d2df1b7b26c1
Jamie Barton <jamie@notrab.dev>
2021-09-10 10:16:50 +0100

    test: check generate signature creates a valid sig

--- index.test.js
+++ index.test.js
@@ -15,3 +15,35 @@ it("should verify the signature", () => {
 
   expect(webhook).toBe(true);
 });
+
+it("should generate a signature", () => {
+  const secret = "rCNwyiloY3oJYYkxgpBXaleIiUv5MYlx";
+
+  const body = {
+    hello: "world",
+  };
+
+  const signature = generateWebhookSignature({ body, secret });
+
+  expect(signature).toContain("sign=");
+  expect(signature).toContain("env=master");
+  expect(signature).toContain("t=");
+});
+
+it("should generate a signature with a custom enviornment name", () => {
+  const secret = "rCNwyiloY3oJYYkxgpBXaleIiUv5MYlx";
+
+  const body = {
+    hello: "world",
+  };
+
+  const signature = generateWebhookSignature({
+    body,
+    secret,
+    environmentName: "test",
+  });
+
+  expect(signature).toContain("sign=");
+  expect(signature).toContain("env=test");
+  expect(signature).toContain("t=");
+});
